e3afff6cf3a53a0353cd044390affeda777665fe
Smith <adam.smith03@cardinalhealth.com>
2020-12-27 11:21:19 -0500

    updated main python file

--- source_code/main.py
+++ source_code/main.py
@@ -1,1 +1,13 @@
-print('Hello, world!')
+print('Welcome to CHOOSE YOUR OWN ADVENTURE!')
+name = input('What is your name? ')
+age = int(input('What is your age? '))
+
+print('Hello', name, 'you are', age, 'years old')
+
+if age >= 18:
+    print('You are old enough to play!')
+    wants_to_play = input('Do you want to play? ')
+    if wants_to_play == "yes":
+        print('Lets play!')
+else:
+    print('Sorry, you are not old enough to play.')
